b7f3da000e4fbfb0eb4baaa839682b6ea245762c
Adham Ismonov <kkirapm@gmail.com>
2024-01-05 13:47:19 +0500

    lecture4

---
+++ homework-9.js
@@ -0,0 +1,29 @@
+// 2
+
+// function t1(n){
+//     let res =n.split(" ");
+//     let find = "";
+//     for(let i = 0; i<res.length; i++){
+//         if(res[i].length>find.length){
+//             find=res[i]
+//         }
+//     }
+//     return find
+// }
+// console.log(t1("Hello darkness my old friend"));
+// console.log(t1("Hello there mate"));
+// console.log(t1("Kayla's toy is plastic"));
+
+// 2
+
+// function t2(a) {
+//     let b = []
+//     for (let i = 0; i < a.length; i++) {
+//         if (a[i].slice(-3) == "man" && a[i].slice(-5) != "Woman") {
+//            b.push(a[i])
+//         }
+        
+//     }
+//     return b
+// }console.log(t2(["Batman", "Superman", "Spider-man", "Hulk", "Wolverine",
+// "Wonder-Woman"]))
